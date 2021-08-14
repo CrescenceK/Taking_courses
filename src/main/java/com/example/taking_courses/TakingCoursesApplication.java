cbbe047d97c9936fdb4eec2de737a485ffce6f08
CrescenceK <kcrescy@gmail.com>
2021-08-14 15:58:38 +0200

    taking_courses project started

---
+++ src/main/java/com/example/taking_courses/TakingCoursesApplication.java
@@ -0,0 +1,15 @@
+package com.example.taking_courses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class TakingCoursesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TakingCoursesApplication.class, args);
+    }
+
+
+}
---
+++ src/test/java/com/example/taking_courses/TakingCoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taking_courses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakingCoursesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
